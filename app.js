c313b7b2861b26500dd6a4e4c72a29faf9839ec2
Eythan Kramm <eythankramm@eythans-mbp.attlocal.net>
2019-05-12 18:45:18 -0400

    finished object literals

--- app.js
+++ app.js
@@ -476,3 +476,45 @@
 ////////////////////////////////////////////////////////
 
 // Object Literals
+
+// const person = {
+//   firstName: "Steve",
+//   lastName: "Smith",
+//   age: 30,
+//   email: "steve@aol.com",
+//   hobbies: ["music", "sports"],
+//   address: {
+//     city: "Miami",
+//     state: "FL"
+//   },
+//   getBirthYear: function() {
+//     return 2017 - this.age;
+//   }
+// };
+
+// let val;
+
+// val = person;
+// Get Specific value
+// val = person.firstName;
+// val = person["firstName"];
+// val = person.age;
+// val = person.hobbies[1];
+// val = person.address.state;
+// val = person.getBirthYear();
+
+// console.log(val);
+
+// const people = [
+//   { name: "John", age: 30 },
+//   { name: "Mike", age: 23 },
+//   { name: "Nancy", age: 41 }
+// ];
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i].name);
+// }
+
+// Object Literals End
+
+////////////////////////////////////////////////////////
