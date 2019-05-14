1c80d2abce5d4f90f029d1b5b9ee90ae1fac5515
Eythan Kramm <eythankramm@eythans-mbp.attlocal.net>
2019-05-14 18:05:16 -0400

    finished window object

--- app.js
+++ app.js
@@ -839,3 +839,68 @@
 // General Loops End
 
 ////////////////////////////////////////////////////////
+
+// Window Object // the globel object in client side JavaScript
+
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+// alert("Hello World");
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm("Are you sure")) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+//
+// let val;
+
+// Outter height and width
+// val = window.outerHeight;
+// val = window.outerWidth;
+
+// Inner height and width
+// val = window.innerHeight;
+// val = window.innerWidth;
+
+// Scroll points
+// val = window.scrollY;
+// val = window.scrollX;
+
+// Location Object
+// val = window.location;
+// val = window.location.hostname;
+// val = window.location.port;
+// val = window.location.href;
+// val = window.location.search;
+
+// Redirect
+// window.location.href = "http://google.com";
+
+// Reload
+// window.location.reload();
+
+// History Object
+// window.history.go(-1);
+// val = window.history.length;
+
+// Navigator Object
+// val = window.navigator;
+// val = window.navigator.appName;
+// val = window.navigator.appVersion;
+// val = window.navigator.userAgent;
+// val = window.navigator.platform;
+// val = window.navigator.vendor;
+// val = window.navigator.langurage;
+
+// console.log(val);
+
+// Window Object End
+
+////////////////////////////////////////////////////////
