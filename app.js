5f2ea7cb6ca68d9fc55a316c45852e3a0f5ccdf1
Eythan Kramm <eythankramm@eythans-mbp.attlocal.net>
2019-05-14 16:35:33 -0400

    finishe general loops

--- app.js
+++ app.js
@@ -759,3 +759,83 @@
 // Function Declarations and Expressions
 
 ////////////////////////////////////////////////////////
+
+// General Loops
+
+//FOR LOOP
+// for (let i = 0; i < 10; i++) {
+//   if (i === 2) {
+//     console.log("2 is my favorit number");
+//     continue;
+//   }
+
+//   if (i === 5) {
+//     console.log("Stop the loop");
+//     break;
+//   }
+//   console.log("Number" + i);
+// }
+
+//WHILE LOOP
+// let i = 0;
+
+// while (i < 10) {
+//   console.log("Number" + i);
+//   i++;
+// }
+
+// DO WHILE
+// let i = 0;
+
+// do {
+//     console.log("Number " + i);
+//     i++;
+// }
+
+// while(i < 10);
+
+// LOOP THROUGH ARRAY
+// const cars = ["Ford", "Chevy", "Honda", "Toyota"];
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+// FOREACH
+// the function in a forEach loop can take in 3 things
+// 1. is the iterator or the objects being iterated through (list of cars a car is the iterator)
+// 2. the index the location in the array 0 based
+// 3. the actual whole array
+// cars.forEach(function(car, index, array) {
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users = [
+//   { id: 1, name: "John" },
+//   { id: 2, name: "Sara" },
+//   { id: 3, name: "Karen" },
+//   { id: 4, name: "Steve" }
+// ];
+
+// const ids = users.map(function(user) {
+//   return user.id;
+// });
+
+// console.log(ids);
+
+// FOR IN
+// const user = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 40
+// };
+
+// for (let x in user) {
+//   console.log(`${x} : ${user[x]}`);
+// }
+
+// General Loops End
+
+////////////////////////////////////////////////////////
